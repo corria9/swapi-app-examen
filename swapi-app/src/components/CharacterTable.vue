940639abde2bc469fa65dc9b45ce5b7e31d2658a
corria9 <corria9@gmail.com>
2022-11-30 14:05:54 -0600

    Resolviendo Issues

--- swapi-app/src/components/CharacterTable.vue
+++ swapi-app/src/components/CharacterTable.vue
@@ -1,24 +1,75 @@
 <template>
-    <table>
-        <tbody>
-            CharacterTable
-        </tbody>
-    </table>
+	<table class="characterList">
+		<TableHeader @search="search" />
+
+		<tbody>
+			<template v-for="(character, index) in characters" :key="index">
+				<TableRow ref="rows" :character="character" />
+			</template>
+		</tbody>
+
+		<tfoot>
+			<Pagination @back="previousPage" @next="nextPage" />
+		</tfoot>
+	</table>
 </template>
 
 <script>
+import Requests from '../Requests.js'
 import TableHeader from '../components/TableHeader.vue'
 import TableRow from '../components/TableRow.vue'
+import Pagination from '../components/Pagination.vue'
 
 export default {
-    name: 'CharacterTable',
-    components: {
-        TableHeader,
-        TableRow,
-    }
+	name: 'CharacterTable',
+	data() {
+		return {
+			characters: [],
+			page: 1,
+			count: 0,
+			searchString: '',
+		}
+	},
+	components: {
+		TableHeader,
+		TableRow,
+		Pagination,
+	},
+	created() {
+		this.updateTable(this.page, this.searchString)
+	},
+	methods: {
+		nextPage() {
+			if (this.page < Math.ceil(this.count / 10)) {
+				this.updateTable(++this.page, this.searchString)
+				this.closeRows()
+			}
+		},
+		previousPage() {
+			if (this.page > 1) {
+				this.updateTable(--this.page, this.searchString)
+				this.closeRows()
+			}
+		},
+		async updateTable(page = 1, search = '') {
+			const data = await Requests.getCharacters(page, search)
+			this.characters = data.results
+			this.count = data.count
+		},
+		search(value = '') {
+			this.searchString = value
+			this.page = 1
+			this.updateTable(this.page, this.searchString)
+			this.closeRows()
+		},
+		closeRows() {
+			for (const row of this.$refs.rows) {
+				row.closeInfo()
+			}
+		},
+	},
 }
 </script>
-
 
 <style scoped>
 table {
---
+++ swapi-app/src/components/Pagination.vue
@@ -0,0 +1,41 @@
+<template>
+	<tr class="pagination">
+		<td>
+			<button @click="pageSwitch('next')">&raquo;</button>
+			<button @click="pageSwitch('back')">&laquo;</button>
+		</td>
+	</tr>
+</template>
+
+<script>
+export default {
+	name: 'Pagination',
+	methods: {
+		pageSwitch(val) {
+			this.$emit(val)
+		},
+	},
+}
+</script>
+
+<style scoped>
+td {
+	border: 2px solid #ffff66;
+	text-align: right;
+}
+
+.pagination button {
+	color: #ffff66;
+	float: right;
+	padding: 8px 16px;
+	font-size: large;
+	text-decoration: none;
+	border: none;
+	background: none;
+	transition: background-color 0.3s;
+}
+
+.pagination button:hover {
+	background-color: rgb(36, 35, 35);
+}
+</style>
